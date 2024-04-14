c0c8879bc6a0bde5e67e6434c729c153c6752705
ayushgemini <ayushgemini13@gmail.com>
2024-04-14 17:06:35 +0530

    - Removed Unused Imports

--- app/src/main/java/com/example/jetpackcomposeandroidtutorial/button/ButtonActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeandroidtutorial/button/ButtonActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -18,13 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.vector.DefaultTintColor
+
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcomposeandroidtutorial.R
 import com.example.jetpackcomposeandroidtutorial.layout.showToast
 
 class ButtonActivity : ComponentActivity() {
